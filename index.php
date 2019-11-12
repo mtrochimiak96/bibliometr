5ee15d995115a4978c62fbe12df40944fa0a84fd
mtrochimiak96 <57639692+mtrochimiak96@users.noreply.github.com>
2019-11-12 12:08:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+	<title>Bibliometr</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">	
+</head>
+<body>
+	
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<ul class="navbar-nav">
+			<li class="nav-item active">
+					<a class="navbar-brand" href="#">Bibliometr</a>
+</li></div>
+
+	<div class="collapse navbar-collapse">
+		<a class="nav-link" href="index.php">Strona Główna</a>
+		</li></div>
+			<li class="nav navbar-nav navbar-right">
+			<a class="nav-link" href="adding.php">Dodaj artykuł</a>
+				<a class="nav-link" href="registration.php">Zarejestruj</a>
+					<a class="nav-link" href="login.php">Zaloguj</a>
+	</li>
+  </ul>
+ </div>
+</nav>
+<br><br>
+	
+	
+<div class="jumbotron">
+  
+<div class="container">
+  <h2>Wyszukiwarka artykułów</h2>
+
+
+  
+</div></div>
+
+
+	
+</body>
+</html>
