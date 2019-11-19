6e4284c9fa228350547266ee06f21581809d1cf4
mtrochimiak96 <57639692+mtrochimiak96@users.noreply.github.com>
2019-11-19 18:00:11 +0100

    Update baza.sql

--- sql/baza.sql
+++ sql/baza.sql
@@ -1,33 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Lis 2019, 23:02
+-- Wersja serwera: 10.4.8-MariaDB
+-- Wersja PHP: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `bibmati`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `article`
+--
 
 CREATE TABLE `article` (
-  `id_article` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(255) NOT NULL,
-  `doi` INT(11) NOT NULL,
-  `minipoint` INT(11),
-  `conference` VARCHAR(80),
-  `publicdate` DATE,
-  `author` INT(11) NOT NULL,
-  PRIMARY KEY (`id_article`)
-) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `id_article` int(11) UNSIGNED NOT NULL,
+  `title` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `doi` int(11) NOT NULL,
+  `minipoint` int(11) DEFAULT NULL,
+  `conference` varchar(80) COLLATE utf8_polish_ci DEFAULT NULL,
+  `publicdate` date DEFAULT NULL,
+  `author` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Zrzut danych tabeli `article`
+--
+
+INSERT INTO `article` (`id_article`, `title`, `doi`, `minipoint`, `conference`, `publicdate`, `author`) VALUES
+(8, 'Maciuś wielki', 0, 2, 'asd', '2019-11-04', 2),
+(9, 'asd', 0, 2, 'asd', '2019-11-04', 0),
+(10, 'asd', 0, 2, 'asd', '2019-11-04', 0),
+(11, 'asd', 0, 2, 'asd', '2019-11-04', 0),
+(12, 'maciek', 2, 2, '4devs', '2019-11-14', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `authors`
+--
 
 CREATE TABLE `authors` (
-   `id_authors` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-   `name` INT(11) NOT NULL,
-   `surname` INT(11) NOT NULL,
-   `participation` VARCHAR(10) NOT NULL,
-   `numberofarticles` INT(10) NOT NULL,
-   PRIMARY KEY (`id_authors`)
-);
+  `id_authors` int(11) UNSIGNED NOT NULL,
+  `name` int(11) NOT NULL,
+  `surname` int(11) NOT NULL,
+  `participation` varchar(10) NOT NULL,
+  `numberofarticles` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
 
 CREATE TABLE `users` (
-     `id_user` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-     `email` VARCHAR(50) NOT NULL,
-     `name` VARCHAR(20) NOT NULL,
-     `surname` VARCHAR(50) NOT NULL,
-     `password` VARCHAR(20),
-     `affilation` VARCHAR(50) NOT NULL,
-     PRIMARY KEY (`id_user`)
-);
+  `id_user` int(11) UNSIGNED NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `affilation` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indeksy dla zrzutów tabel
+--
 
+--
+-- Indeksy dla tabeli `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id_article`);
+
+--
+-- Indeksy dla tabeli `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id_authors`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `article`
+--
+ALTER TABLE `article`
+  MODIFY `id_article` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT dla tabeli `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id_authors` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
